ce7dcb4bf3634ebeb64c8ef4e013d8bce1cb5728
emmettwu <emmettwu@outlook.com>
2014-05-24 19:08:37 +0800

    add code to install ibus and octave

--- config.sh
+++ config.sh
@@ -1,8 +1,10 @@
 clear
-sudo apt-get update
-sudo apt-get install chromium-browser git vim-gnome
-sudo apt-get install texlive texlive-xetex vim-latexsuite
+apt-get update
+apt-get install chromium-browser git vim-gnome
+apt-get install texlive texlive-xetex vim-latexsuite
 vim-addons install latex-suite
-sudo apt-get ipython3 ipython3-notebook python3-numpy python3-scipy
+apt-get ipython3 ipython3-notebook python3-numpy python3-scipy
 cp -r ./.vim/ ~/
 cp ./.vimrc ~/
+apt-get install ibus-googlepinyin
+apt-get install octave
