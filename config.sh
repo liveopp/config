82676cc0e0334d86efc7c4d9ca2bc6f8650eafc6
emmettwu <emmettwu@outlook.com>
2014-05-24 18:56:17 +0800

    add command to install python and copy files

--- config.sh
+++ config.sh
@@ -1,3 +1,8 @@
 clear
 sudo apt-get update
-sudo apt-get install chromium-browser git vim-gnome vim-latexsuite
+sudo apt-get install chromium-browser git vim-gnome
+sudo apt-get install texlive texlive-xetex vim-latexsuite
+vim-addons install latex-suite
+sudo apt-get ipython3 ipython3-notebook python3-numpy python3-scipy
+cp -r ./.vim/ ~/
+cp ./.vimrc ~/
